02f257d54751e8f5972d18694ce73b2fee0e26be
Mau <itehi000@gmail.com>
2025-04-26 16:20:55 -0600

    Case sensitive arreglado para los adelantos en JS

--- videos.js
+++ videos.js
@@ -4,35 +4,35 @@ var misVideos = [
     imagen: "../miniaturas/ColiMiau (6).png",
     fecha: "23-09-2024",
     enlace: "https://www.youtube.com/watch?v=ruaz9P7Zvqk&t=149s",
-    adelanto: "/Colimiau-pagina/minis-video/elden-ring.gif",
+    adelanto: "/colimiau-pagina/minis-video/elden-ring.gif",
   },
   {
     titulo: "😱 PEGANDO CARTELES EN LA CALLE 😱",
     imagen: "../miniaturas/ColiMiau (4).png",
     fecha: "16-06-2024",
     enlace: "https://www.youtube.com/watch?v=LvzCU-5jcSs",
-    adelanto: "/Colimiau-pagina/minis-video/pegando-carteles.gif",
+    adelanto: "/colimiau-pagina/minis-video/pegando-carteles.gif",
   },
   {
     titulo: "⛏️🧱Minecraft HARDCORE es IMPOSIBLE🧱⛏️",
     imagen: "../miniaturas/ColiMiau (3).png",
     fecha: "03-06-2024",
     enlace: "https://www.youtube.com/watch?v=fu2HOK43y_k&t=3s",
-    adelanto: "/Colimiau-pagina/minis-video/minecraft-1.gif",
+    adelanto: "/colimiau-pagina/minis-video/minecraft-1.gif",
   },
   {
     titulo: "🔥👹DUO INSANO juega por PRIMERA VEZ FREE FIRE👹🔥",
     imagen: "../miniaturas/ColiMiau (2).png",
     fecha: "25-05-2024",
     enlace: "https://www.youtube.com/watch?v=jkflGwAQB4I&t=3s",
-    adelanto: "/Colimiau-pagina/minis-video/free-fire-1.gif",
+    adelanto: "/colimiau-pagina/minis-video/free-fire-1.gif",
   },
   {
     titulo: "🚗🏜️CONDUCIENDO en el DESIERTO con ALEXCRAFT🚗🏜️",
     imagen: "../miniaturas/ColiMiau (1).png",
     fecha: "21-05-2024",
     enlace: "https://www.youtube.com/watch?v=QZ97lCPVzK0&t=4s",
-    adelanto: "/Colimiau-pagina/minis-video/desierto-roblox.gif",
+    adelanto: "/colimiau-pagina/minis-video/desierto-roblox.gif",
   },
   {
     titulo:
@@ -40,6 +40,6 @@ var misVideos = [
     imagen: "../miniaturas/ColiMiau.png",
     fecha: "14-05-2024",
     enlace: "https://www.youtube.com/watch?v=WmYAy2Wl7ZI",
-    adelanto: "/Colimiau-pagina/minis-video/LOL-1.gif",
+    adelanto: "/colimiau-pagina/minis-video/LOL-1.gif",
   },
 ];
